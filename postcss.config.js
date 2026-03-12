ec2145f62044833a5055b24fb0c17252355c1faf
Artyomkun <selemetartem@gmail.com>
2026-03-12 16:33:20 +0300

    Clean up project: remove build files and unnecessary folders

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,3 @@ export default {
     autoprefixer: {}
   }
 };
-<<<<<<< HEAD
-
-module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
-  ]
-};
-=======
->>>>>>> 6d9ece145be331bb2f202013f3c4ed3b01bd3cd1
